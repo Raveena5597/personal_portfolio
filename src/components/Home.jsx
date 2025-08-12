efd686917bef0bcfabd4d9d1de898de5858d6ba4
Raveena <raveena.putta@gmail.com>
2025-08-12 11:07:37 +0530

    changed the home page content along with ads,animation

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -49,6 +49,17 @@ export default function Home() {
         />
       )}
 
+      {/* blobs Background */}
+      <div className="animated-bg">
+        <div className="blob blob1"></div>
+        <div className="blob blob2"></div>
+        <div className="blob blob3"></div>
+        <div className="blob blob4"></div>
+        <div className="blob blob5"></div>
+        <div className="blob blob6"></div>
+        <div className="blob blob7"></div>
+      </div>
+
       {/* Hero Section */}
       <div
         id="home"
